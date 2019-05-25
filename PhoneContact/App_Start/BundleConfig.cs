bbe9cd87502ae8e18c36cac01db210b38b1d2c08
ramazankaplan.co <ramazankaplan.co@gmail.com>
2019-05-26 01:50:04 +0300

    Updated project to, Bootstrap4.3+, JQuery3.4+, TypeScript3.4+ and Target ES2019. TargetFramework 4.8+

--- PhoneContact/App_Start/BundleConfig.cs
+++ PhoneContact/App_Start/BundleConfig.cs
@@ -6,30 +6,39 @@ using System.Web.Optimization;
 
 namespace PhoneContact
 {
-	public class BundleConfig
-	{
-		// For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
-		public static void RegisterBundles(BundleCollection bundles)
-		{
-			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-						"~/Scripts/jquery-{version}.js"));
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
 
-			// Use the development version of Modernizr to develop with and learn from. Then, when you're
-			// ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-						"~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/popper").Include(
+                        "~/Scripts/umd/popper.js"));
 
-			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-					  "~/Scripts/bootstrap.js"));
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
 
-			bundles.Add(new ScriptBundle("~/bundles/customScript").Include(
-					  "~/Scripts/customScript/employee.js",
-					  "~/Scripts/customScript/user.js", 
-					  "~/Scripts/customScript/department.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
 
-			bundles.Add(new StyleBundle("~/Content/css").Include(
-					  "~/Content/bootstrap.css",
-					  "~/Content/site.css"));
-		}
-	}
+            bundles.Add(new ScriptBundle("~/bundles/employee").Include(
+                "~/Scripts/customScript/employee.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/user").Include(
+                "~/Scripts/customScript/user.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/department").Include(
+                      "~/Scripts/customScript/department.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            //BundleTable.EnableOptimizations = true;
+        }
+    }
 }
